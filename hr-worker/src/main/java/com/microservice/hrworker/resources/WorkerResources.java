c382640116b37eae2f9ff0d23250c68484b3782d
Bruuno96 <brunotkd96@gmail.com>
2022-03-02 14:00:46 -0300

    load balance with ribbon of spring cloud

--- hr-worker/src/main/java/com/microservice/hrworker/resources/WorkerResources.java
+++ hr-worker/src/main/java/com/microservice/hrworker/resources/WorkerResources.java
@@ -2,7 +2,10 @@ package com.microservice.hrworker.resources;
 
 import com.microservice.hrworker.entities.Worker;
 import com.microservice.hrworker.repository.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,11 +18,17 @@ import java.util.List;
 @RequestMapping("/workers")
 public class WorkerResources {
 
+    private static Logger logger = LoggerFactory.getLogger(WorkerResources.class);
     @Autowired
     private WorkerRepository workerRepository;
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
+
+        logger.info("PORT = "+environment.getProperty("local.server.port"));
         Worker found = workerRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Object not found"));
         return ResponseEntity.ok().body(found);
     }
