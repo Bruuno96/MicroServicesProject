1269337290cf9bf7be8c4f27a23e5af2a8145cac
Bruuno96 <brunotkd96@gmail.com>
2022-03-02 20:36:53 -0300

    project hr-api-gateway-zuul

---
+++ api-gatewayzuul/src/main/java/com/bruno/apigatewayzuul/ApiGatewayzuulApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.apigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayzuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayzuulApplication.class, args);
+    }
+
+}
---
+++ api-gatewayzuul/src/test/java/com/bruno/apigatewayzuul/ApiGatewayzuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.apigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayzuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
