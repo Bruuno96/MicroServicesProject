bd3f32f9229b6ef4a17d96fe583fff998a6ffb8e
Bruuno96 <brunotkd96@gmail.com>
2022-03-02 16:57:17 -0300

    random port to hr-worker

--- payroll/src/main/java/com/bruno/payroll/PayrollApplication.java
+++ payroll/src/main/java/com/bruno/payroll/PayrollApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class PayrollApplication {
