9396fc4c4a03d934c12f6888a0787d7ffc1d8736
Bruuno96 <brunotkd96@gmail.com>
2022-03-03 15:28:23 -0300

    Inserts Db

--- hr-user/src/main/java/com/bruno/hruser/entities/Role.java
+++ hr-user/src/main/java/com/bruno/hruser/entities/Role.java
@@ -2,10 +2,12 @@ package com.bruno.hruser.entities;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 import java.util.Objects;
 
 @Entity
+@Table(name="tb_role")
 public class Role implements Serializable {
 
     private static final long serialVersionUID = 1L;
