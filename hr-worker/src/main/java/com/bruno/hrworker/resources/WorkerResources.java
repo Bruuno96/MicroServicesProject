b25445c7fc979e7d4bc923458cd3c59134b3fc97
Bruuno96 <brunotkd96@gmail.com>
2022-03-03 12:58:27 -0300

    actuator runtime config refresh

--- hr-worker/src/main/java/com/bruno/hrworker/resources/WorkerResources.java
+++ hr-worker/src/main/java/com/bruno/hrworker/resources/WorkerResources.java
@@ -6,6 +6,8 @@ import com.bruno.hrworker.repository.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,17 +17,29 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RefreshScope
 @RestController
 @RequestMapping("/workers")
 public class WorkerResources {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerResources.class);
 
+    @Value("${test.config}")
+    private String testConfig;
+
+
+
     @Autowired
     private WorkerRepository workerRepository;
 
     @Autowired
     private Environment environment;
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfigs() {
+        logger.info("CONFIG = " + testConfig);
+        return ResponseEntity.noContent().build();
+    }
 
     @GetMapping("{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
