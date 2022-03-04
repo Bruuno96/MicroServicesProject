3c79f7dfd5481cf60bd01ca44347c5d46819270f
Bruuno96 <brunotkd96@gmail.com>
2022-03-04 03:04:48 -0300

    jwt part 1

---
+++ hr-oauth/src/main/java/com/bruno/hroauth/config/AppConfig.java
@@ -0,0 +1,30 @@
+package com.bruno.hroauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    // Configuração da string de assinatura
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter(){
+        JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+        tokenConverter.setSigningKey("MY-SECRET-KEY");
+        return tokenConverter;
+    }
+
+    // Responsável por ler as informaçoes do token
+    @Bean
+    public JwtTokenStore jwtTokenStore(){
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+}
