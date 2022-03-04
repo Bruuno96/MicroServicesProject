95079f5f9a8a6b15b3cc2c44fc901e2faa18987e
Bruuno96 <brunotkd96@gmail.com>
2022-03-04 11:19:58 -0300

    jwt part 2

--- hr-oauth/src/main/java/com/bruno/hroauth/service/UserServic.java
+++ hr-oauth/src/main/java/com/bruno/hroauth/service/UserServic.java
@@ -3,19 +3,28 @@ package com.bruno.hroauth.service;
 import com.bruno.hroauth.entities.User;
 import com.bruno.hroauth.feignClients.UserFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServic {
+public class UserServic implements UserDetailsService {
 
     @Autowired
     private UserFeignClient userFeignClient;
-
 
     public User findByEmail(String email){
         User byEmail = userFeignClient.findByEmail(email).getBody();
         if(byEmail == null) throw new IllegalArgumentException("Email not found");
         return byEmail;
 
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User byEmail = userFeignClient.findByEmail(username).getBody();
+        if(byEmail == null) throw new UsernameNotFoundException("Email not found");
+        return byEmail;
     }
 }
